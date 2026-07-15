c39c15328096943c0b8f7c98eca21985487cef9d
MatteV02 <veroni.matteo@icloud.com>
2026-07-15 12:18:17 +0200

    fixed content

--- src/content.config.ts
+++ src/content.config.ts
@@ -23,4 +23,79 @@ const globals = defineCollection({
   }),
 });
 
-export const collections = { globals };
+const pages = defineCollection({
+  loader: glob({ pattern: "**/*.yaml", base: "./src/content/pages" }),
+  schema: ({ image }) =>
+    z.object({
+      intro: z.object({
+        text: z.string(),
+      }),
+      work: z
+        .object({
+          label: z.string().optional(),
+          title: z.string().optional(),
+          projects: z.array(
+            z.object({
+              year: z.string(),
+              title: z.string(),
+              description: z.string(),
+              category: z.string(),
+              logo: image().optional(),
+              image: image(),
+              imageAlt: z.string(),
+            })
+          ),
+        })
+        .optional(),
+      projects: z
+        .object({
+          label: z.string().optional(),
+          title: z.string().optional(),
+          items: z.array(
+            z.object({
+              year: z.string(),
+              title: z.string(),
+              description: z.string(),
+              category: z.string(),
+              logo: image().optional(),
+              image: image(),
+              imageAlt: z.string(),
+            })
+          ),
+        })
+        .optional(),
+      education: z
+        .object({
+          label: z.string().optional(),
+          title: z.string().optional(),
+          items: z.array(
+            z.object({
+              period: z.string(),
+              title: z.string(),
+              description: z.string(),
+              category: z.string(),
+            })
+          ),
+        })
+        .optional(),
+      skills: z
+        .object({
+          label: z.string().optional(),
+          title: z.string().optional(),
+          items: z.array(
+            z.object({
+              period: z.string(),
+              title: z.string(),
+              description: z.string(),
+              category: z.object({
+                label: z.string(),
+                href: z.string(),
+              }),
+            })
+          ),
+        })
+        .optional(),
+    }),
+});
+
+export const collections = { globals, pages };
